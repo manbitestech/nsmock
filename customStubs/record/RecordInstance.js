7ce64f5f9d04d0640b67cd28c1d1f7359f8af32f
Patrick B <pb@Patricks-MacBook-Air.local>
2025-07-27 21:46:32 -0500

    Record class: fleshing out methods.

--- customStubs/record/RecordInstance.js
+++ customStubs/record/RecordInstance.js
@@ -4,35 +4,68 @@ class Record {
         const {objData} = opt
         this.type = objData.header.type
         this.id = objData.header.id
-        this._fields = objData.fields
+        this._fields = objData.fields || {}
         this._sublists = objData.sublists || {}
         this._subrecords = objData.subrecords || {}
-        this._buildGetValue = function(getText) {
-            const finalKey = getText === true ? 'text' : 'value'
-            const builtFunction = function(opt) {
-                return this._fields[opt.fieldId]?.[finalKey]
-            }
-            return builtFunction
-        }
         this.getValue = this._buildGetValue(false)
         this.getText = this._buildGetValue(true)
+        this.getSublistValue = jest.fn(this._buildGetSublistValue(false))
+        this.getSublistText = this._buildGetSublistValue(true)
+        this.setSublistValue = jest.fn() // todo: store values
+        this.setValue = jest.fn(this._buildSetValue())
+        this.insertLine = jest.fn(function(opt) {
+            if (opt.sublistId === undefined) {
+                throw {"message": "sublistId not supplied"}
+            } 
+            if (this._sublists[opt.sublistId] === undefined) {
+                this._sublists[opt.sublistId] = []
+            }
+            const insertedLine = {}
+            // todo: if it is a subrecord, we need to create a new record instance
+            this._sublists[opt.sublistId].splice(opt.line, 0, insertedLine)
+        })
+        this.save = jest.fn(function() {
+            if (this.id === undefined && this._id !== undefined) {
+                this.id = this._id // ID is hidden until saved.
+            }
+            return this.id
+        });
     }
-    getSublistValue(opt) {
-        if (opt.sublistId === undefined) {
-            throw "sublistId not supplied"
+    _buildGetValue = function(getText) {
+            const finalKey = getText === true ? 'text' : 'value'
+            return function(opt) {
+                return this._fields[opt.fieldId]?.[finalKey]
+            }
         }
-        if (opt.fieldId === undefined) {
-            throw "fieldId not supplied"
-        }
-        if (typeof opt.line !== 'number') {
-            throw "line not supplied or non-numerical"
-        }
-        const sub = this._sublists[opt.sublistId]
-        if (sub === undefined) {
-            throw "Sublist not initialized."
-        }
-        return sub[opt.line]?.[opt.fieldId]?.value
+    _buildGetSublistValue(getText) {
+        return function(opt) {
+            const finalKey = getText === true ? 'text' : 'value'
+            if (opt.sublistId === undefined) {
+                throw {"message": "sublistId not supplied"}
+            }
+            if (opt.fieldId === undefined) {
+                throw {"message": "fieldId not supplied"}
+            }
+            if (typeof opt.line !== 'number') {
+                throw {"message": "line not supplied or non-numerical"}
+            }
+            const sub = this._sublists[opt.sublistId];
+            if (sub === undefined) {
+                throw "Sublist not initialized.";
+            }
+            return sub[opt.line]?.[opt.fieldId]?.[finalKey]
+        };
     }
+    _buildSetValue() {
+        // Ability to later add a setText method
+        return function (opt) {
+            if (this._fields[opt.fieldId] === undefined) {
+                this._fields[opt.fieldId] = {}
+            }
+            this._fields[opt.fieldId].value = opt.value;
+        }
+    }
+    
 }
 
 module.exports = {
