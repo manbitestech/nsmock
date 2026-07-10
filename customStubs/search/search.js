8cf5545c9c63c343231ea0564753cd58a570c544
pb <pcb313@pm.me>
2026-07-10 16:02:36 -0500

    combining search concepts.

--- customStubs/search/search.js
+++ customStubs/search/search.js
@@ -3,7 +3,8 @@ const nsSearchDefault = require('@oracle/suitecloud-unit-testing/stubs/search/se
 
 class nsMockSearch {
     constructor() {
-        this._results = {};
+        this._resultSet = new ResultSet(inputJson)
+        this._getResultSet = function() { return this._resultSet }
 
         this._setResults = (type, results) => {
             this._results[type] = results.map(r => ({
@@ -47,4 +48,43 @@ class nsMockSearch {
     }
 }
 
-module.exports = new nsMockSearch();
+//module.exports = new nsMockSearch();
+
+
+class Search {
+  _resultSet = {}
+  _recordLookupListObjects = []
+
+  /**
+   * Outputs a mock object with an instance.each method that can iterate and return the search results line by line.
+   * @param {Array} inputJson Array of records to be output. JSON structure given above.
+   * Please see 'nsmock-oop-test' and other tests for examples of usage.
+   */
+  constructor (inputJson) {
+    this._resultSet = new ResultSet(inputJson)
+  }
+
+  _getResultSet () { return this._resultSet }
+
+  /**
+   * @description given a lookup result, with an id and type
+   * @param {{id: number, type: String, key:[{value: number, text: String}]}}object
+   */
+  addLookupFieldData = (object) => {
+    this._recordLookupListObjects.push(object)
+  }
+
+  run = jest.fn(() => this._getResultSet())
+  runPaged = jest.fn(() => this._getResultSet())
+  lookupFields = jest.fn((options) => {
+    const foundValues = this._recordLookupListObjects.filter((x) => x.id === options.id && x.type === options.type)
+    const pick = (o, ...props) => {
+      return Object.assign({}, ...props.map(prop => ({[prop]: o[prop]})));
+    };
+
+    return foundValues.map((v, i, a) => { return pick(v, options.columns)})[0]
+  })
+}
+
+module.exports = Search
+
