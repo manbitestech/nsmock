71fb6045d8b6ee286ec50e345d42046ee7ac47e3
pb <pcb313@pm.me>
2026-07-23 13:27:00 -0500

    update _precreate to support record types; do _init for multiple tests in files.

--- customStubs/record/record.js
+++ customStubs/record/record.js
@@ -16,6 +16,11 @@ class nsMockRecord {
         this._database = []
     }
 
+    _init() {
+        this._database = []
+        this._cancreate = {}
+    }
+
     _preload(recordArray) {
         recordArray.forEach(rec => {
             // Needs Type and Id. ID will be unavailable until the record is saved.
@@ -27,19 +32,44 @@ class nsMockRecord {
     }
 
 
-    _precreate(recordArray) {
-        recordArray.forEach(rec => {
-            // Needs Type and Id. ID will be unavailable until the record is saved.
-            if (rec.type === undefined || rec._id === undefined) {
-                throw new Error("Record must have type and hidden id ('_id:1234')");
-            }
-
-            if (this._cancreate[rec.type] === undefined) {
-                this._cancreate[rec.type] = []
-            }
-
-            this._cancreate[rec.type].push(rec)
-        })
+    _precreate(recordData) {
+        // Support both array format (legacy) and object format {type: [records]}
+        // Object format keys can be strings ('customer') or record.Type enums (record.Type.CUSTOMER)
+        if (Array.isArray(recordData)) {
+            // Legacy: array of records
+            recordData.forEach(rec => {
+                if (rec.type === undefined || rec._id === undefined) {
+                    throw new Error("Record must have type and hidden id ('_id:1234')");
+                }
+                if (this._cancreate[rec.type] === undefined) {
+                    this._cancreate[rec.type] = []
+                }
+                this._cancreate[rec.type].push(rec)
+            })
+        } else {
+            // New: object format {[record.Type.CUSTOMER]: [rec1, rec2], [record.Type.SALES_ORDER]: [rec3]}
+            Object.keys(recordData).forEach(type => {
+                const records = recordData[type]
+                if (!Array.isArray(records)) {
+                    throw new Error(`_precreate: value for type '${type}' must be an array`)
+                }
+                // Normalize type key - handle both 'customer' and record.Type.CUSTOMER
+                const typeKey = type.toLowerCase()
+                if (this._cancreate[typeKey] === undefined) {
+                    this._cancreate[typeKey] = []
+                }
+                records.forEach(rec => {
+                    if (rec._id === undefined) {
+                        throw new Error(`Record of type '${type}' must have hidden id ('_id:1234')`);
+                    }
+                    // Set type if not already set
+                    if (rec.type === undefined) {
+                        rec.type = typeKey
+                    }
+                    this._cancreate[typeKey].push(rec)
+                })
+            })
+        }
     }
 
     
